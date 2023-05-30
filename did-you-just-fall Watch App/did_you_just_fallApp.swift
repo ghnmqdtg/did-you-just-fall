0adddf394caed884f6d8abacd92c909932097de9
ghnmqdtg <ghnmqdtg@gmail.com>
2023-05-30 19:13:48 +0800

    Initial Commit

---
+++ did-you-just-fall Watch App/did_you_just_fallApp.swift
@@ -0,0 +1,17 @@
+//
+//  did_you_just_fallApp.swift
+//  did-you-just-fall Watch App
+//
+//  Created by Tweizh on 2023/5/30.
+//
+
+import SwiftUI
+
+@main
+struct did_you_just_fall_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
